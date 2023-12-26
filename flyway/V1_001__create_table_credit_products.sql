2835735d765e6fc599daa0ef09fd986d971aa628
VorobevOleg <kuruzuza-09@ya.ru>
2023-12-26 16:57:43 +0300

    Add docker-compose & PostgreSQL + FlyWay

---
+++ flyway/V1_001__create_table_credit_products.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS credit_products (
+    id                      serial  primary key,                -- id
+    name                    text    unique          not null,   -- Название кредитного продукта
+    interest_rate           numeric(6,4)            not null,   -- Процентная ставка кредитного продукта
+    amount_min              numeric(19,4)           not null,   -- Минимально возможная сумма кредитного продукта
+    amount_max              numeric(19,4)           not null,   -- Максимально возможная сумма кредитного продукта
+    min_duration_months     int                     not null,   -- Минимальный срок, на который можно взять кредитный продукт, в месяцах
+    max_duration_months     int                     not null,   -- Максимальный срок, на который можно взять кредитный продукт, в месяцах
+    is_revocable            boolean                 not null,   -- Возможность досрочного погашения (да/нет)
+    is_guarantee            boolean                 not null,   -- Необходимость наличия поручителей (да/нет)
+    description             text,                               -- Дополнительное описание кредитного продукта
+    image                   text,                               -- Ссылка на файл изображения кредитного продукта
+    is_active               boolean                 not null    -- Активность кредитного продукта (да/нет)
+    );
+
+--Для быстрого поиска по имени кредитного продукта
+CREATE EXTENSION pg_trgm;
+CREATE INDEX IF NOT EXISTS credit_products_idx_name ON credit_products USING GIN (name gin_trgm_ops);
